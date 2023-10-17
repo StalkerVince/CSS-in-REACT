eea2beb668304883f8513b49352070a9e98def3c
Thierry Vicente <119328995+StalkerVince@users.noreply.github.com>
2023-10-17 09:53:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./component/Card";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Card />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Card.jsx
@@ -0,0 +1,20 @@
+import icon from "../assets/icon.png";
+
+function Card() {
+  return (
+    <body>
+      <div className="page">
+        <img src={icon} />
+        <h1>Les Ornithorynques</h1>
+        <h2>Mi-Canard, Mi-Castor: Une anomalie. </h2>
+        <p>Ses chevilles ont des aiguillons venimeux non-létaux.</p>
+        <p>
+          Sa fourrure emet de la lumière bleue quand il est exposé aux
+          Ultra-Violets
+        </p>
+      </div>
+    </body>
+  );
+}
+
+export default Card;
